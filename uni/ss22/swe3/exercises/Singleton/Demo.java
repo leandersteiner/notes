f547de472ff2650c0351e5bc92f0c7b311c923ab
Leander Steiner <leandersteiner@yahoo.de>
2022-03-20 15:52:49 +0100

    Reformat broken formatting.

--- uni/ss22/swe3/exercises/Singleton/Demo.java
+++ uni/ss22/swe3/exercises/Singleton/Demo.java
@@ -1,21 +1,20 @@
 public class Demo {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
+    StandardInterestRate test = StandardInterestRate.getInstance();
+    System.out.println(test.getInterestRate());
+    test.setInterestRate(5.0);
+    System.out.println(test.getInterestRate());
 
-		StandardInterestRate test = StandardInterestRate.getInstance();
-		System.out.println(test.getInterestRate());
-		test.setInterestRate(5.0);
-		System.out.println(test.getInterestRate());
-
-		StandardInterestRate test2 = StandardInterestRate.getInstance();
-		System.out.println(test2.getInterestRate());
+    StandardInterestRate test2 = StandardInterestRate.getInstance();
+    System.out.println(test2.getInterestRate());
 
     EnumExample test3 = EnumExample.INSTANCE;
     System.out.println(test3.getInterestRate());
-		test3.setInterestRate(5.0);
-		System.out.println(test3.getInterestRate());
+    test3.setInterestRate(5.0);
+    System.out.println(test3.getInterestRate());
 
-		EnumExample test4 = EnumExample.INSTANCE;
-		System.out.println(test4.getInterestRate());
-	}
+    EnumExample test4 = EnumExample.INSTANCE;
+    System.out.println(test4.getInterestRate());
+  }
 }
